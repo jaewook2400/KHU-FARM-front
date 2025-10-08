89715c25242c5d6f7761efe69f1d5f6965243adc
jaewook2400 <101032947+jaewook2400@users.noreply.github.com>
2025-10-08 18:32:24 +0900

    version update

--- khu_farm/android/app/build.gradle.kts
+++ khu_farm/android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         minSdk = flutter.minSdkVersion
         ndkVersion = "28.0.13004108"
         targetSdk = flutter.targetSdkVersion
-        versionCode = 16
-        versionName = "1.0.6"
+        versionCode = 17
+        versionName = "1.0.7"
     }
 
     signingConfigs {
